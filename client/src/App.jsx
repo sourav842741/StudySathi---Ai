da1b43eab4ecbffc3bf80690e65f4c0676b648f0
Sourav kumar <161188554+sourav842741@users.noreply.github.com>
2026-02-09 20:37:18 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,7 +9,7 @@ import Notes from './pages/Notes'
 import Pricing from './pages/Pricing'
 import PaymentSuccess from './pages/PaymentSuccess'
 import PaymentFailed from './pages/PaymentFailed'
-export const serverUrl = "https://studysathi-aiserver.onrender.com"
+export const serverUrl = "https://studysathi-ai.onrender.com"
 
 function App() {
   const dispatch = useDispatch()
